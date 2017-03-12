a6d91acd5d8a0b24c45cfda92821926cd1505711
espirus <mike@espirus.com>
2017-03-12 23:39:31 +0000

    Update search.js

--- search.js
+++ search.js
@@ -1,1 +1,18 @@
-// workshop code goes in here :)
+/var search = instantsearch({
+  appId: 'latency',
+  apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
+  indexName: 'movies',
+  urlSync: {}
+});/ workshop code goes in here :)
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#q'
+  })
+);
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    hitsPerPage: 10,
+  })
+);
+search.start();
