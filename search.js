431a27cd70c7a2842880cbd728452add768ddc2c
espirus <mike@espirus.com>
2017-03-13 00:19:39 +0000

    Update search.js

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
 var search = instantsearch({
   appId: 'latency',
-  apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
+  apiKey: '7e2574f14ffa553c0683b80e299e279a',
   indexName: 'movies',
   urlSync: {}
 });
